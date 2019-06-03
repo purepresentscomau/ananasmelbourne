ba3dbef4e65591b6af112677fec063a6096fdeae
Mithilesh Domala <mithileshd27@users.noreply.github.com>
2019-06-04 02:02:50 +1000

    Update index.html

--- js/snd_email.js
+++ js/snd_email.js
@@ -1,21 +1,5 @@
 $(function() {
   $("#submit_btn").on("click", function(){
-    var myInputs = ["form_name","form_lastname","form_email", "form_phone", "form_date"];
-    var myFormData = [];   //declare an array
-    myInputs.forEach(function(element) {
-      myFormData.push(document.getElementById(element).value);
-    })
-    if($("#form_bday_yes").is(':checked')) {
-      myFormData.push("bday_yes");
-    } else {
-      myFormData.push("bday_no");
-    }
-    if($("#form_booth_yes").is(':checked')) {
-      myFormData.push("booth_yes");
-    } else {
-      myFormData.push("booth_no");
-    }
-    console.log(myFormData);
-    alert("Form Submitted. Thank You.");
+    alert("Form Submitted. Thank You. Click OK and close the next tab.");
   })
 })
