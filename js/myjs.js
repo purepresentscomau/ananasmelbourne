b453ac50d441d209946848926a1d0108bc809166
Mithilesh Domala <mithileshd27@users.noreply.github.com>
2019-07-12 02:29:56 +1000

    update index.html

--- js/myjs.js
+++ js/myjs.js
@@ -2,8 +2,12 @@ $(window).bind('DOMContentLoaded load resize', function () {
   if ($(window).innerWidth() > $(window).innerHeight() ) {
     $(".large_sunday").removeClass("large_sunday").addClass("small_sunday");
     $(".larger_logo_size").removeClass("larger_logo_size").addClass("large_logo_size");
+    document.getElementById("arrow").style.display = "block";
+    document.getElementById("spacerdiv").style.marginBottom = "40px";
   } else {
     $(".large_logo_size").removeClass("large_logo_size").addClass("larger_logo_size");
     $(".small_sunday").removeClass("small_sunday").addClass("large_sunday");
+    document.getElementById("arrow").style.display = "none";
+    document.getElementById("spacerdiv").style.marginBottom = "0px";
   }
 });
